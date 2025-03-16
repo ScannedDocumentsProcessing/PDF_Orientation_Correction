14f381ab95c945c4825adccbaabac4fd10380244
Roncal Jossef <47817632+RoncalJossef@users.noreply.github.com>
2025-03-16 10:35:06 +0100

    adding oritentation and rotate

--- models/image.py
+++ models/image.py
@@ -3,7 +3,9 @@ from interfaces.orientationpredictor import OrientationPredictor
 class Image:
     def __init__(self, raw_data):
         self.__raw_data = raw_data
-        self.__orientation_prediction = 0
+        self.__orientation = 0
     
     def predict_orientation(self, predictor: OrientationPredictor):
-        self.__orientation_prediction = predictor.process(self.__raw_data)
+        result_prediction = predictor.process(self.__raw_data)
+        self.__orientation = result_prediction["orientation"]
+        self.__rotate = result_prediction["rotate"]
--- services/tesseractorientationpredictor.py
+++ services/tesseractorientationpredictor.py
@@ -5,5 +5,7 @@ class TesseractOrientationPredictor[OrientationPredictor]:
     
     def process(self, raw_img):
         osd_result = pytesseract.image_to_osd(raw_img, output_type=Output.DICT)
-        print(osd_result)
-        return osd_result
+        result = {}
+        result["orientation"] = osd_result['orientation']*90
+        result["rotate"] = osd_result['rotate']
+        return result
